719f98fc91c03826d042baa1d0266184170a6859
Adebayo Fawaz <lordrisk@gmai.com>
2021-12-26 08:52:44 +0100

    latest

--- dist/index.045664b0.js
+++ dist/index.045664b0.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector('.header-burger');
+const closebtn = document.querySelector('.nav-close');
 const nav = document.querySelector('.nav');
 const toggeNav = ()=>{
     nav?.classList.toggle('nav-show');
 };
 burger?.addEventListener('click', toggeNav);
+closebtn?.addEventListener('click', toggeNav);
 
 //# sourceMappingURL=index.045664b0.js.map
--- src/script/script.ts
+++ src/script/script.ts
@@ -1,4 +1,5 @@
 const burger = document.querySelector('.header-burger');
+const closebtn = document.querySelector('.nav-close');
 const nav = document.querySelector('.nav');
 
 const toggeNav = () => {
@@ -6,3 +7,4 @@ const toggeNav = () => {
 };
 
 burger?.addEventListener('click', toggeNav);
+closebtn?.addEventListener('click', toggeNav);
